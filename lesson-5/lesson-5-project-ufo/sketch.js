c5d6a996d2a4e1c19b13693b8eb7d4f6c2b95fe1
Dan Weiner <daweiner16@gmail.com>
2018-04-21 15:16:00 -0400

    add gameover

--- lesson-5/lesson-5-project-ufo/sketch.js
+++ lesson-5/lesson-5-project-ufo/sketch.js
@@ -1,6 +1,8 @@
 let ufo;
 let bullets = [];
 
+let gameOver = false;
+
 function setup() {
 	createCanvas(400, 400);
 	ufo = new UFO();
@@ -10,18 +12,26 @@ function setup() {
 function draw() {
 	background(150);
 
-	ufo.display();
-	ufo.move();
+	if (gameOver) {
+		textSize(48);
+		textAlign(CENTER);
+		fill(0);
+		text('GAME OVER', width/2, height/2);
+	} else {
+		ufo.display();
+		ufo.move();
 
-	for (var i = 0; i < bullets.length; i++) {
-		if (!bullets[i].isFinished) {
-			bullets[i].display();
-			bullets[i].move();
-			if (bullets[i].intersect(ufo)) {
-				bullets[i].finished();
-			}
-			if (bullets[i].reachedTop()) {
-				bullets[i].finished();
+		for (var i = 0; i < bullets.length; i++) {
+			if (!bullets[i].isFinished) {
+				bullets[i].display();
+				bullets[i].move();
+				if (bullets[i].intersect(ufo)) {
+					bullets[i].finished();
+					gameOver = true;
+				}
+				if (bullets[i].reachedTop()) {
+					bullets[i].finished();
+				}
 			}
 		}
 	}
