ad5c28602a76a4ba5ee64ae34f4093a2040b8341
Dan Weiner <daweiner16@gmail.com>
2018-04-21 10:57:44 -0400

    add bullet display and move functionality

--- lesson-5/lesson-5-project-ufo/sketch.js
+++ lesson-5/lesson-5-project-ufo/sketch.js
@@ -1,34 +1,54 @@
-let x;
-let y;
+let ufoX;
+let ufoY;
 let xspeed = -2;
-let isFinished = false;
+let ufoIsFinished = false;
+
+let bulletX;
+let bulletY;
+let bulletIsFinished = false;
 function setup() {
 	createCanvas(400, 400);
-	x = width-50;
-	y = 0;
+	ufoX = width-50;
+	ufoY = 0;
+
+	bulletX = width/2;
+	bulletY = height;
 }
 
 function draw() {
 	background(150);
 	displayUFO();
 	moveUFO();
+
+	// create bullet
+	fill(0);
+	ellipse(bulletX, bulletY, 10);
+
+	// move bullet as long as bullet hasnt reached top
+	if (!bulletIsFinished) {
+		bulletY -= 1;
+		if (bulletY < 0) {
+			bulletIsFinished = true;
+		}
+	}
 }
 
 function displayUFO() {
 	fill(0);
-	rect(x, y, 50, 30)
+	rect(ufoX, ufoY, 50, 30)
 }
 
 function moveUFO() {
-	if (!isFinished) {
-		x = x + xspeed;
-		if (x < 0 || x > width - 50) {
+	if (!ufoIsFinished) {
+		ufoX = ufoX + xspeed;
+		if (ufoX < 0 || ufoX > width - 50) {
 			xspeed *= -1;
-			y+= 30;
+			ufoY += 30;
 		}
-	} if (y > height - 50) {
-		y = height - 50;
-		isFinished = true;
+	} if (ufoY > height - 50) {
+		ufoY = height - 50;
+		ufoIsFinished = true;
 	}
 
 }
+
