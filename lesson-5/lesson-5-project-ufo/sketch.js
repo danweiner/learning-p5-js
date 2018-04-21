585eed67b7f02ad340bb26c7c9cca81df841943e
Dan Weiner <daweiner16@gmail.com>
2018-04-21 12:27:48 -0400

    mousepressed bullets added to array

--- lesson-5/lesson-5-project-ufo/sketch.js
+++ lesson-5/lesson-5-project-ufo/sketch.js
@@ -3,18 +3,11 @@ let ufoY;
 let xspeed = -2;
 let ufoIsFinished = false;
 
-let bulletX;
-let bulletY;
-let bulletIsFinished = false;
-
-let bullet;
+let bullets = [];
 function setup() {
 	createCanvas(400, 400);
 	ufoX = width-50;
 	ufoY = 0;
-
-	// bulletX = width/2;
-	// bulletY = height;
 
 	bullet = new Bullet();
 
@@ -24,20 +17,12 @@ function draw() {
 	background(150);
 	displayUFO();
 	moveUFO();
-
-	// create bullet
-	// fill(0);
-	// ellipse(bulletX, bulletY, 10);
-
-	// // move bullet as long as bullet hasnt reached top
-	// if (!bulletIsFinished) {
-	// 	bulletY -= 1;
-	// 	if (bulletY < 0) {
-	// 		bulletIsFinished = true;
-	// 	}
-	// }
-	bullet.display();
-	bullet.move();
+	for (var i = 0; i < bullets.length; i++) {
+		bullets[i].display();
+		bullets[i].move();
+	}
+	// bullet.display();
+	// bullet.move();
 }
 
 function displayUFO() {
@@ -59,14 +44,17 @@ function moveUFO() {
 
 }
 
+function mousePressed() {
+	let bullet = new Bullet();
+	bullets.push(bullet);
+}
+
 class Bullet {
 	constructor() {
 		this.x = width/2;
 		this.y = height;
 		this.w = 10;
 		this.speed = 2;
-
-		// this.isFinished = false;
 	}
 
 	display() {
@@ -74,14 +62,7 @@ class Bullet {
 		ellipse(this.x, this.y, this.w);
 	}
 
-	move() {
-		// // this.y -= this.speed;
-		// if (!this.isFinished) {
-		// 	this.y -= this.speed;
-		// 	if (this.y < 0) {
-		// 		this.isFinished = true;
-		// 	}
-		// }
+	move() {	
 		this.y -= this.speed;
 		if (this.y < 0) {
 			this.y = 0;
