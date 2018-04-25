bf590f66e7c78f59ae0edb6b0422ae6c7f9b0c40
Dan Weiner <daweiner16@gmail.com>
2018-04-25 16:36:16 -0400

    add spiral

---
+++ lesson-6/exercise-13.05-spiral-path/sketch.js
@@ -0,0 +1,22 @@
+let r = 0;
+let theta = 0;
+
+function setup() {
+	createCanvas(200, 200);
+	background(255);
+}
+
+function draw() {
+	let x = r * cos(theta);
+	let y = r * sin(theta);
+
+	// Draw an ellipse at x,y
+	noStroke();
+	fill(0);
+	ellipse(x+width/2, y + height/2, 16, 16);
+
+	// Increment the angle
+	theta += .01;
+	r += .1;
+
+}
